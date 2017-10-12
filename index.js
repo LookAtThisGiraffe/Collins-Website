e855098e71ef04b7c67d2aa683c61e916d2cba07
Collin <Cflannery@student.csdspartans.org>
2017-10-12 13:30:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ function openCity(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+<script>
+var audio = new Audio("http://music.ogg" ) ;
+
+audio.oncanplaythrough = function(){
+audio.play();
+}
+
+audio.loop = true;
+
+audio.onended = function(){
+audio.play();
+}
+
+</script>
+
+<input type="image" src="http://button.png" onclick="audio.play()">
